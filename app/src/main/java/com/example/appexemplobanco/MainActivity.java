93a1eb18d50003157a408f113ee5b7513113b9d5
totemarcal <totemarcal@gmail.com>
2020-11-26 22:12:37 -0300

    atualizacao

--- app/src/main/java/com/example/appexemplobanco/MainActivity.java
+++ app/src/main/java/com/example/appexemplobanco/MainActivity.java
@@ -1,17 +1,23 @@
-package com.example.appexemplobanco;
+package com.example.exemplobancosqlitequi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Parcelable;
 import android.util.SparseBooleanArray;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import com.example.appexemplobanco.Produto;
+import com.example.appexemplobanco.ProdutoActivity;
+import com.example.appexemplobanco.ProdutoDAO;
+import com.example.appexemplobanco.R;
+
+import java.io.Serializable;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,13 +29,42 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        /*Button btNovo = (Button) findViewById(R.id.btCadastrar);
-        btNovo.setOnClickListener(new View.OnClickListener() {
+        Button btDetalhe = (Button) findViewById(R.id.btDetalhe);
+        btDetalhe.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), ProdutoActivity.class));
+                SparseBooleanArray checked = listView.getCheckedItemPositions();
+                for (int i = 0; i < checked.size(); i++){
+                    Produto produto = (Produto) listView.getItemAtPosition(checked.keyAt(i));
+                    //pega os itens marcados
+                    Produto prod = produtoHelper.getProdutoByID(produto.getId());
+                    Intent it = new Intent(getApplicationContext(), ProdutoActivity.class);
+                    it.putExtra("id", -1);
+                    it.putExtra("nome", prod.getNome());
+                    it.putExtra("preco", prod.getPreco());
+                    it.putExtra("quantidade", prod.getQuantidade());
+                    startActivity(it);
+                }
             }
-        });*/
+        });
+        Button btAlterar = (Button) findViewById(R.id.btalterar);
+        btAlterar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SparseBooleanArray checked = listView.getCheckedItemPositions();
+                for (int i = 0; i < checked.size(); i++){
+                    Produto produto = (Produto) listView.getItemAtPosition(checked.keyAt(i));
+                    //pega os itens marcados
+                    Produto prod = produtoHelper.getProdutoByID(produto.getId());
+                    Intent it = new Intent(getApplicationContext(), ProdutoActivity.class);
+                    it.putExtra("id", prod.getId());
+                    it.putExtra("nome", prod.getNome());
+                    it.putExtra("preco", prod.getPreco());
+                    it.putExtra("quantidade", prod.getQuantidade());
+                    startActivity(it);
+                }
+            }
+        });
     }
 
     @Override
@@ -67,4 +102,5 @@ public class MainActivity extends AppCompatActivity {
     public void onCLickNovo(View view) {
         startActivity(new Intent(this, ProdutoActivity.class));
     }
+
 }
--- app/src/main/java/com/example/appexemplobanco/ProdutoActivity.java
+++ app/src/main/java/com/example/appexemplobanco/ProdutoActivity.java
@@ -4,30 +4,58 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.appexemplobanco.Produto;
+import com.example.appexemplobanco.ProdutoDAO;
+import com.example.appexemplobanco.R;
+
 public class ProdutoActivity extends AppCompatActivity {
-    private ProdutoDAO produtoHelper;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produto);
+        if (getIntent().getExtras()!=null){
+            EditText edtNome = (EditText) findViewById(R.id.edtNome);
+            EditText edtPreco = (EditText ) findViewById(R.id.edtPreco);
+            EditText edtQuantidade = (EditText) findViewById(R.id.edtQuantidade);
+            edtNome.setText(getIntent().getExtras().getString("nome"));
+            edtPreco.setText(Float.toString(getIntent().getExtras().getFloat("preco")));
+            edtQuantidade.setText(Integer.toString(getIntent().getExtras().getInt("quantidade")));
+            if(getIntent().getExtras().getInt("id")==-1) {
+                Button bt = (Button) findViewById(R.id.btnSalvar);
+                edtNome.setEnabled(false);
+                edtPreco.setEnabled(false);
+                edtQuantidade.setEnabled(false);
+                bt.setText("Voltar");
+            }
+        }
     }
 
     public void onClick(View view) {
         try{
-            produtoHelper = new ProdutoDAO(this);
-            EditText edtNome = (EditText) findViewById(R.id.edtNome);
-            EditText edtPreco = (EditText ) findViewById(R.id.edtPreco);
-            EditText edtQuantidade = (EditText) findViewById(R.id.edtQuantidade);
-            Produto produto = new Produto();
-            produto.setNome(edtNome.getText().toString());
-            produto.setPreco(Float.parseFloat(edtPreco.getText().toString()));
-            produto.setQuantidade(Integer.parseInt(edtQuantidade.getText().toString()));
-            produtoHelper.inserirProduto(produto);
+            Button bt = (Button) findViewById(R.id.btnSalvar);
+            if(!bt.getText().toString().equals("Voltar")) {
+                ProdutoDAO produtoHelper = new ProdutoDAO(this);
+                EditText edtNome = (EditText) findViewById(R.id.edtNome);
+                EditText edtPreco = (EditText) findViewById(R.id.edtPreco);
+                EditText edtQuantidade = (EditText) findViewById(R.id.edtQuantidade);
+                Produto produto = new Produto();
+                produto.setNome(edtNome.getText().toString());
+                produto.setPreco(Float.parseFloat(edtPreco.getText().toString()));
+                produto.setQuantidade(Integer.parseInt(edtQuantidade.getText().toString()));
+                if(getIntent().getExtras().getInt("id")!=-1) {
+                    produto.setId(getIntent().getExtras().getInt("id"));
+                    produtoHelper.alterarProduto(produto);
+                }else{
+                    produtoHelper.inserirProduto(produto);
+                }
 
-            Toast.makeText(this, "Cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Cadastrado com sucesso!", Toast.LENGTH_SHORT).show();
+            }
             this.finish();
         }catch (Exception e){
             e.getStackTrace();
